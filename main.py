cd7bed85e3068d85c81c7f2ff8d0014237fc3672
유병훈 <isac322@naver.com>
2015-10-09 03:01:48 +0900

    restructure codes

--- main.py
+++ main.py
@@ -1,49 +1,10 @@
+from NetworkTool import down_file, login, get_soup
+
 __author__ = 'isac3'
 
-from urllib.request import urlopen, Request
-from urllib.parse import urlencode
-import http.client
-import urllib.error
-import sys
 import os
-import re
-
-from bs4 import BeautifulSoup as Bs
 
 import Tools
-
-
-def get_soup(url, query=None):
-	response = get_response(url, query)
-	ret = Bs(response, 'html.parser')
-	response.close()
-	return ret
-
-
-def get_response(url, query=None, header=None):
-	if header is None:
-		user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
-		header = {
-			'Connection': 'keep-alive',
-			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-			'Upgrade-Insecure-Requests': '1',
-			'User-Agent': user_agent,
-			'Content-Type': 'application/x-www-form-urlencoded'
-		}
-
-	data = None
-	if query is not None:
-		data = urlencode(query).encode('utf-8')
-
-	req = Request(url, data, header)
-
-	try:
-		response = urlopen(req)
-		return response
-
-	except urllib.error.HTTPError:
-		print('invalid username')
-		sys.exit(1)
 
 
 def get_solved_problems(sp):
@@ -59,7 +20,7 @@ working_dir = ''
 
 
 def make_code_file(problem_num, language):
-	extension = Tools.get_extension(language)
+	extension = get_extension(language)
 
 	file_name = problem_num + '.' + extension
 	directory = os.path.join(working_dir, language)
@@ -120,63 +81,19 @@ def get_submitted_files(problems):
 			file.close()
 
 
-def login(user_name, pw):
-	query = {'login_user_id': user_name, 'login_password': pw, 'auto_login': 'on'}
-	data = urlencode(query)
-	header = {
-		'Host': 'www.acmicpc.net',
-		'Connection': 'keep-alive',
-		'Cache-Control': 'max-age=0',
-		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-		'Origin': 'https://www.acmicpc.net',
-		'Upgrade-Insecure-Requests': '1',
-		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
-		'Content-Type': 'application/x-www-form-urlencoded',
-		'Referer': 'https://www.acmicpc.net/login/?next=/',
-		'Accept-Encoding': 'gzip, deflate',
-		'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4',
-	}
-
-	conn = http.client.HTTPSConnection("www.acmicpc.net")
-	conn.request('POST', '/signin', data, header)
-	response = conn.getresponse()
-
-	cookie = ''
-	patt = re.compile('^.*((__cfduid|OnlineJudge|acmicpcautologin)[=][\w\d]+;).*$', re.MULTILINE)
-
-	for line in patt.finditer(str(response.info())):
-		cookie += line.group(1) + ' '
-
-	response.close()
-	# print(response.msg)
-
-	return cookie
-
-
-def down_file(judge_id, cookie):
-	header = {
-		'Host': 'www.acmicpc.net',
-		'Connection': 'keep-alive',
-		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-		'Origin': 'https://www.acmicpc.net,.',
-		'Upgrade-Insecure-Requests': '1',
-		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
-		'Content-Type': 'application/x-www-form-urlencoded',
-		'Referer': 'https://www.acmicpc.net/source/' + judge_id,
-		'Accept-Encoding': 'gzip, deflate',
-		'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4',
-		'Cookie': cookie + ' _gauges_unique_day=1; _gauges_unique_month=1; _gauges_unique_year=1; _gauges_unique=1; _ga=GA1.2.1918118912.1444272664'
-	}
-
-	conn = http.client.HTTPSConnection("www.acmicpc.net")
-	conn.request('POST', '/source/download/' + judge_id, None, header)
-
-	response = conn.getresponse()
-	code = response.read()
-
-	response.close()
-
-	return code
+def get_extension(language):
+	if language in ['C++', 'C++11']:
+		return 'cpp'
+	elif language in ['C']:
+		return 'c'
+	elif language in ['Python', 'Python3', 'PyPy']:
+		return 'py'
+	elif language in ['Java']:
+		return 'java'
+	elif language in ['Text']:
+		return 'txt'
+	elif language in ['PHP']:
+		return 'php'
 
 
 if __name__ == '__main__':
