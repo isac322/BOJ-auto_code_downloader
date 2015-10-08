21a78e1ef8a191d02c5b34bcf4c1bc2425910272
유병훈 <isac322@naver.com>
2015-10-09 02:55:21 +0900

    roll back file name for avoid collision

--- tools.py
+++ tools.py
@@ -2,16 +2,15 @@ __author__ = 'isac3'
 
 
 class Problem:
-	def __init__(self, judge_id, mem, time, lang, code_len):
+	def __init__(self, judge_id, mem, time, code_len=None):
 		self.judge_id = judge_id
 		self.memory_size = mem
 		self.time = time
-		self.language = lang
 		self.code_length = code_len
 
 	def __lt__(self, other):
 		if self.time == other.time:
-			if self.memory_size == other.memory_size:
+			if self.memory_size == other.memory_size and self.code_length and other.code_length:
 				return self.code_length < other.code_length
 			else:
 				return self.memory_size < other.memory_size
@@ -19,14 +18,25 @@ class Problem:
 			return self.time < other.time
 
 	def __str__(self):
-		return "{0} {1}KB {2}MS {3} {4}B".format(self.judge_id, self.memory_size, self.time, self.language,
-												self.code_length)
+		if self.code_length:
+			return "{0} {1} KB {2} MS {3} B".format(self.judge_id, self.memory_size, self.time, self.code_length)
+		else:
+			return "{0} {1} KB {2} MS".format(self.judge_id, self.memory_size, self.time)
 
 	def __repr__(self):
 		return self.__str__()
 
 
-if __name__ == '__main__':
-	a = Problem('a', 'b', 'c', 'd', 'e')
-
-	print(a.code_length)
+def get_extension(language):
+	if language in ['C++', 'C++11']:
+		return 'cpp'
+	elif language in ['C']:
+		return 'c'
+	elif language in ['Python', 'Python3', 'PyPy']:
+		return 'py'
+	elif language in ['Java']:
+		return 'java'
+	elif language in ['Text']:
+		return 'txt'
+	elif language in ['PHP']:
+		return 'php'
